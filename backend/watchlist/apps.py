1e1272d11d8360961cf018941ffca88f6b2a433b
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-15 17:38:57 +0530

    Start watch list scheduler with app lifecycle

--- backend/watchlist/apps.py
+++ backend/watchlist/apps.py
@@ -1,6 +1,14 @@
+import os
+
 from django.apps import AppConfig
 
 
 class WatchlistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "watchlist"
+
+    def ready(self):
+        if os.environ.get("PWMS_DISABLE_BACKGROUND_SCHEDULERS") == "1":
+            return
+        from watchlist.services.scheduler import WatchListScheduler
+        WatchListScheduler.start()

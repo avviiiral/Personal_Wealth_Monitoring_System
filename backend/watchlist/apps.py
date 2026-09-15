0d17467f75ba2ba7367923d145ffe90affb8f5bb
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-15 17:40:47 +0530

    Keep Watch List app lifecycle side-effect free

--- backend/watchlist/apps.py
+++ backend/watchlist/apps.py
@@ -1,15 +1,6 @@
-import os
-import sys
-
 from django.apps import AppConfig
 
 
 class WatchlistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "watchlist"
-
-    def ready(self):
-        if os.environ.get("PWMS_DISABLE_BACKGROUND_SCHEDULERS") == "1" or "test" in sys.argv:
-            return
-        from watchlist.services.scheduler import WatchListScheduler
-        WatchListScheduler.start()

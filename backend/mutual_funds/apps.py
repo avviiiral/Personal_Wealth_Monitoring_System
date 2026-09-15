5aaa018382f7ba3c4c878fdd83abe19d83700cb2
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-15 15:12:46 +0530

    Start mutual fund underlying scheduler

--- backend/mutual_funds/apps.py
+++ backend/mutual_funds/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
+from config.scheduler_guard import should_start_background_schedulers
+
 
 class MutualFundsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mutual_funds'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mutual_funds"
+
+    def ready(self):
+        if not should_start_background_schedulers():
+            return
+
+        from .services.underlying_scheduler import MutualFundUnderlyingScheduler
+
+        MutualFundUnderlyingScheduler.start()

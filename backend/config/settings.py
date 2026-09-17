698e56eb02a70ce36517f7b564af31c230436bb6
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-17 11:55:29 +0530

    Fix Django messages middleware path

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -73,8 +73,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'

830d338306a7448e2a8b4a350d5eabaa88f361b3
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-17 10:22:59 +0530

    Fix settings middleware path after PostgreSQL configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 

e47df049c5209fa188dafdfae8f603422ad0f161
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-18 18:30:30 +0530

    Make analytics family scope authoritative

--- backend/analytics/services/xirr_by_asset_class.py
+++ backend/analytics/services/xirr_by_asset_class.py
@@ -26,7 +26,7 @@ class XIRRByAssetClassService:
         if family is None:
             return Q(pk__in=[])
         owner_ids = XIRRByAssetClassService._owner_ids(user)
-        return Q(family_id=family.id) | Q(family_id__isnull=True, owner_id__in=owner_ids)
+        return Q(family_id=family.id)
 
     @staticmethod
     def _owner_ids(user):

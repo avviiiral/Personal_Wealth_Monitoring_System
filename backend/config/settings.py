dff6859d33ef1f68148b52711c4c1cf6ab87c08d
Aviral Goyal <aviralgoyal739@gmail.com>
2026-09-22 11:17:18 +0530

    Restore authenticated default API permissions

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,7 +152,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.SessionAuthentication'],
 }
 
